da048cf344119300f04d2bd788d37eaf138b13fd
Jupiter Bakakeu <jupiter.bakakeu@gmail.com>
2021-12-08 18:44:30 +0100

    Add routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,31 @@
-import Vue from "vue";
-import Router from "vue-router";
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
 
 const routerOptions = [
-  { path: "/", component: "Home" },
-  { path: "/inbound", component: "Inbound" },
-  { path: "/production", component: "Production" },
-  // { path: "/outbound", component: "Home", meta: { requiresAuth: true } },
-  { path: "/outbound", component: "Outbound" },
-  { path: "/cockpit", component: "Cockpit" },
-  { path: "/notifications", component: "Notifications" },
-  { path: "/settings", component: "Settings" },
-  { path: "/help", component: "Help" },
-  { path: "*", component: "NotFound" }
+    { path: "/", component: "Home" },
+    { path: "/inbound", component: "Inbound" },
+    { path: "/production", component: "Production" },
+    // { path: "/outbound", component: "Home", meta: { requiresAuth: true } },
+    { path: "/outbound", component: "Outbound" },
+    { path: "/cockpit", component: "Cockpit" },
+    { path: "/notifications", component: "Notifications" },
+    { path: "/settings", component: "Settings" },
+    { path: "/help", component: "Help" },
+    { path: "*", component: "NotFound" }
 ];
-
+  
 const routes = routerOptions.map(route => {
-  return {
-    ...route,
-    component: () => import(`../components/${route.component}.vue`)
-  };
+    return {
+        ...route,
+        component: () => import(`../views/${route.component}.vue`)
+    };
 });
 
-Vue.use(Router);
+const router = new VueRouter({
+    mode: 'history',
+    routes
+})
 
-export default new Router({
-  mode: "history",
-  routes
-});
+export default router;
